4ca918763144006f54c34932df9e4eb0c5bc0001
Daniel Janus <dj@danieljanus.pl>
2019-10-18 14:57:57 +0200

    Add reading separator lists and some FSA.

--- src/fuszerom/dictionary.clj
+++ src/fuszerom/dictionary.clj
@@ -25,6 +25,29 @@
         tags (repeatedly num #(read-tag buf))]
     (into {} tags)))
 
+(defn read-separators-list [buf]
+  (let [num (Short/toUnsignedInt (.getShort buf))]
+    (vec (repeatedly num #(.getInt buf)))))
+
+(defn read-segrules-fsa-options [buf]
+  (let [num (Byte/toUnsignedLong (.get buf))]
+    (into {} (repeatedly num #(vector (read-null-terminated-string buf)
+                                      (read-null-terminated-string buf))))))
+
+(defn read-segrules-fsa [buf]
+  (let [size (.getInt buf)]
+    ;; TODO: deserialize the FSA
+    (.position buf (+ (.position buf) size))
+    size))
+
+(defn read-segrules-fsa-entry [buf]
+  [(read-segrules-fsa-options buf)
+   (read-segrules-fsa buf)])
+
+(defn read-segrules-fsa-map [buf]
+  (let [num (Byte/toUnsignedLong (.get buf))]
+    (into {} (repeatedly num #(read-segrules-fsa-entry buf)))))
+
 (def magic-number (unchecked-int 0x8fc2bc1b))
 (def version-number 21)
 (def version-number-offset 4)
@@ -40,16 +63,23 @@
             (throw (ex-info "Not a dictionary file" {:filename filename})))
         _ (.position buffer fsa-data-size-offset)
         fsa-data-size (.getInt buffer)
-        _ (.position buffer (+ fsa-data-offset fsa-data-size 4))
+        epilogue-offset (+ fsa-data-offset fsa-data-size)
+        _ (.position buffer epilogue-offset)
+        a1-size (.getInt buffer) ; FIXME: proper name
         id (read-null-terminated-string buffer)
         copyright (read-null-terminated-string buffer)
         tagset-id (read-null-terminated-string buffer)
         tags (read-tags buffer)
         names (read-tags buffer)
-        labels (read-tags buffer)]
+        labels (read-tags buffer)
+        _ (.position buffer (+ epilogue-offset a1-size 4))
+        separators-list (read-separators-list buffer)
+        segrules-fsa-map (read-segrules-fsa-map buffer)]
     {:id id
      :copyright copyright
      :tagset-id tagset-id
      :tags tags
      :names names
-     :labels labels}))
+     :labels labels
+     :separators-list separators-list
+     :segrules-fsa-map segrules-fsa-map}))
