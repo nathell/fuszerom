f212778fbab3cefcf62957cc0efef20e7419462c
Daniel Janus <dj@danieljanus.pl>
2021-05-06 00:12:24 +0200

    Typehint ByteBuffer-accessing functions, rewrite reading segrules.

--- src/fuszerom/dictionary.clj
+++ src/fuszerom/dictionary.clj
@@ -1,7 +1,9 @@
 (ns fuszerom.dictionary
   (:require
-    [clojure.java.io :as io]
-    [nio.core :as nio]))
+   [clojure.java.io :as io]
+   [nio.core :as nio])
+  (:import
+   [java.nio ByteBuffer]))
 
 (defn slurp-bytes [f]
   (let [file (io/file f)]
@@ -13,62 +15,79 @@
           (.read stream arr 0 len))
         arr))))
 
-(defn read-null-terminated-string [buf]
+(defn read-null-terminated-string [^ByteBuffer buf]
   (String. (into-array Byte/TYPE (take-while (comp not zero?) (repeatedly #(.get buf))))))
 
-(defn read-tag [buf]
+(defn read-tag [^ByteBuffer buf]
   [(Short/toUnsignedInt (.getShort buf))
    (read-null-terminated-string buf)])
 
-(defn read-tags [buf]
+(defn read-tags [^ByteBuffer buf]
   (let [num (Short/toUnsignedInt (.getShort buf))
         tags (repeatedly num #(read-tag buf))]
     (into {} tags)))
 
-(defn read-separators-list [buf]
+(defn read-separators-list [^ByteBuffer buf]
   (let [num (Short/toUnsignedInt (.getShort buf))]
     (vec (repeatedly num #(.getInt buf)))))
 
-(defn read-segrules-fsa-options [buf]
+(defn read-segrules-fsa-options [^ByteBuffer buf]
   (let [num (Byte/toUnsignedLong (.get buf))]
     (into {} (repeatedly num #(vector (read-null-terminated-string buf)
                                       (read-null-terminated-string buf))))))
 
-(defn read-segrules-initial-transitions [arr]
-  (let [buf (nio/byte-buffer arr)
-        _ (.position buf 1)
-        num-transitions (.get buf)]
-    (into {}
-          (repeatedly num-transitions (fn []
-                                        (let [index (.get buf)
-                                              shift (.get buf)
-                                              offset (.getShort buf)
-                                              b1 (aget arr offset)
-                                              b2 (aget arr (inc offset))
-                                              accepting (bit-test b1 0)
-                                              weak (bit-test b1 1)
-                                              sink (zero? b2)
-                                              failed (and sink (not accepting))]
-                                          [index {:shift (not= shift 0)
-                                                  :offset offset
-                                                  :accepting accepting
-                                                  :weak weak
-                                                  :sink sink
-                                                  :failed failed}]))))))
+(defmacro with-marked-position [buf & body]
+  `(let [buf# ~buf
+         saved-pos# (.position buf#)]
+     (try
+       ~@body
+       (finally
+         (.position buf# saved-pos#)))))
 
-(defn read-segrules-fsa [buf]
+(defn slice [^ByteBuffer buf ^long size]
+  (let [res (.slice buf)]
+    (.limit res size)
+    res))
+
+(defn segrules-entry [^ByteBuffer buf]
+  (let [segnum (.get buf)
+        shift (.get buf)
+        offset (Short/toUnsignedInt (.getShort buf))
+        [b1 b2] (with-marked-position buf
+                  (.position buf offset)
+                  [(.get buf) (.get buf)])
+        accepting (bit-test b1 0)
+        weak (bit-test b1 1)
+        sink (zero? b2)
+        failed (and sink (not accepting))]
+    {:segnum segnum
+     :shift (not (zero? shift))
+     :offset offset
+     :accepting accepting
+     :weak weak
+     :sink sink
+     :failed failed}))
+
+(defn segrules-entries [^ByteBuffer buf ^long offset]
+  (.position buf (inc offset))
+  (let [num-entries (Byte/toUnsignedLong (.get buf))]
+    (vec (repeatedly num-entries #(segrules-entry buf)))))
+
+(defn read-segrules-fsa [^ByteBuffer buf]
   (let [size (.getInt buf)
-        arr (byte-array size)]
+        image (slice buf size)]
+    (.position buf (+ (.position buf) size))
     ;; TODO: deserialize the FSA more fully
-    (.get buf arr)
-    {:image arr
-     :initial-transitions (read-segrules-initial-transitions arr)}))
+    {:image image
+     :initial-transitions (->> (segrules-entries image 0)
+                               (map (juxt :segnum identity))
+                               (into {}))}))
 
-(defn read-segrules-fsa-entry [buf]
+(defn read-segrules-fsa-entry [^ByteBuffer buf]
   [(read-segrules-fsa-options buf)
    (read-segrules-fsa buf)])
 
-(defn read-segrules-fsa-map [buf]
+(defn read-segrules-fsa-map [^ByteBuffer buf]
   (let [num (Byte/toUnsignedLong (.get buf))]
     (into {} (repeatedly num #(read-segrules-fsa-entry buf)))))
 
@@ -137,4 +156,6 @@
   (def dict (load-dictionary "../vendor/Morfeusz/dict/sgjp-a.dict"))
   (def fbuf (nio/byte-buffer (:fsa dict)))
   (.position fbuf 257)
-  (read-cfsa1-all-transitions fbuf))
+  (read-cfsa1-all-transitions fbuf)
+  (def buf (-> dict :segrules-fsa-map (get {"aggl" "strict", "praet" "split"}) :image))
+  (segrules-entries buf 8340))
